d8a705a5b672885142168eed5ae68a75662a6b08
Robert Cisek <r.cisek@o2.pl>
2017-12-17 17:05:02 +0100

    Dodano zmianne oraz warunek do rendera

--- src/index.js
+++ src/index.js
@@ -39,8 +39,14 @@ class Board extends React.Component {
     }
 
     render() {
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
-        const status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
